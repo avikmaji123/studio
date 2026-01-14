7b80a3ae622eb02b005f7ebaf059f676f864a402
Avik <avikmaji911@gmail.com>
2026-01-14 05:20:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-48678256-85a10",
+  "appId": "1:887486104045:web:8989dfe852dfdb44c0e7e4",
+  "apiKey": "AIzaSyBj1wBPL32W7-SKYz94Zwaah_3EU_XkC4M",
+  "authDomain": "studio-48678256-85a10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "887486104045"
+};
