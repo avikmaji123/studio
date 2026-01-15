3afbcdbd80e99301cf00a16ce5db10dbf1e3cfda
Avik <avikmaji911@gmail.com>
2026-01-15 04:40:46 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "studio-48678256-85a10",
   "appId": "1:887486104045:web:8989dfe852dfdb44c0e7e4",
   "apiKey": "AIzaSyBj1wBPL32W7-SKYz94Zwaah_3EU_XkC4M",
   "authDomain": "studio-48678256-85a10.firebaseapp.com",
+  "storageBucket": "studio-48678256-85a10.appspot.com",
   "measurementId": "",
   "messagingSenderId": "887486104045"
 };
